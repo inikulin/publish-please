eaf3862a7383cca9cc712cf19c6307ad9f1afbdc
inikulin <ifaaan@gmail.com>
2015-12-06 19:38:12 +0300

    Add uncommitted changes check

--- test/test.js
+++ test/test.js
@@ -2,12 +2,13 @@ var assert      = require('assert');
 var PluginError = require('gulp-util').PluginError;
 var del         = require('del');
 var publish     = require('../lib');
-var git         = require('../lib').git;
+var writeFile   = require('fs').writeFileSync;
+var cmd         = require('../lib').cmd;
 
 before(function () {
     publish.testMode = true;
 
-    return git('clone https://github.com/inikulin/publish-please-test-repo.git').then(function () {
+    return cmd('git clone https://github.com/inikulin/publish-please-test-repo.git').then(function () {
         process.chdir('publish-please-test-repo');
     });
 });
@@ -18,12 +19,13 @@ after(function (done) {
 });
 
 it('Should validate package.json existence', function () {
-    return git('checkout no-package-json')
+    return cmd('git checkout no-package-json')
         .then(function () {
             return publish({
-                confirm:        false,
-                validateGitTag: false,
-                validateBranch: false
+                confirm:          false,
+                checkUncommitted: false,
+                validateGitTag:   false,
+                validateBranch:   false
             });
         })
         .then(function () {
@@ -37,11 +39,12 @@ it('Should validate package.json existence', function () {
 
 describe('Branch validation', function () {
     it('Should expect `master` branch by default', function () {
-        return git('checkout no-tag')
+        return cmd('git checkout no-tag')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: false
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false
                 });
             })
             .then(function () {
@@ -54,12 +57,13 @@ describe('Branch validation', function () {
     });
 
     it('Should validate branch passed via parameter', function () {
-        return git('checkout master')
+        return cmd('git checkout master')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: false,
-                    validateBranch: 'no-tag'
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false,
+                    validateBranch:   'no-tag'
                 });
             })
             .then(function () {
@@ -72,12 +76,13 @@ describe('Branch validation', function () {
     });
 
     it('Should expect the latest commit in the branch', function () {
-        return git('checkout a4b76ae5d285800eadcf16e60c75edc33071d929')
+        return cmd('git checkout a4b76ae5d285800eadcf16e60c75edc33071d929')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: false,
-                    validateBranch: 'master'
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false,
+                    validateBranch:   'master'
                 });
             })
             .then(function () {
@@ -90,23 +95,25 @@ describe('Branch validation', function () {
     });
 
     it('Should pass validation', function () {
-        return git('checkout no-tag')
+        return cmd('git checkout no-tag')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: false,
-                    validateBranch: 'no-tag'
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false,
+                    validateBranch:   'no-tag'
                 });
             });
     });
 
     it('Should not validate if option is disabled', function () {
-        return git('checkout no-tag')
+        return cmd('git checkout no-tag')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: false,
-                    validateBranch: false
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false,
+                    validateBranch:   false
                 });
             });
     });
@@ -114,12 +121,13 @@ describe('Branch validation', function () {
 
 describe('Git tag validation', function () {
     it('Should expect git tag to match version', function () {
-        return git('checkout tag-doesnt-match-version')
+        return cmd('git checkout tag-doesnt-match-version')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: true,
-                    validateBranch: false
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   true,
+                    validateBranch:   false
                 });
             })
             .then(function () {
@@ -132,12 +140,13 @@ describe('Git tag validation', function () {
     });
 
     it('Should expect git tag to exist', function () {
-        return git('checkout no-tag')
+        return cmd('git checkout no-tag')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: true,
-                    validateBranch: false
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   true,
+                    validateBranch:   false
                 });
             })
             .then(function () {
@@ -150,23 +159,79 @@ describe('Git tag validation', function () {
     });
 
     it('Should pass validation', function () {
-        return git('checkout master')
+        return cmd('git checkout master')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: true,
-                    validateBranch: false
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   true,
+                    validateBranch:   false
                 });
             });
     });
 
     it('Should not validate if option is disabled', function () {
-        return git('checkout tag-doesnt-match-version')
+        return cmd('git checkout tag-doesnt-match-version')
             .then(function () {
                 return publish({
-                    confirm:        false,
-                    validateGitTag: false,
-                    validateBranch: false
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false,
+                    validateBranch:   false
+                });
+            });
+    });
+});
+
+describe('Uncommitted changes check', function () {
+
+    afterEach(function () {
+        return cmd('git reset --hard HEAD');
+    });
+
+    it('Should expect no uncommitted changes in the working tree', function () {
+        return cmd('git checkout master')
+            .then(function () {
+                writeFile('README.md', 'Yo!');
+
+                return publish({
+                    confirm:          false,
+                    checkUncommitted: true,
+                    validateGitTag:   false,
+                    validateBranch:   false
+                });
+            })
+            .then(function () {
+                throw new Error('Promise rejection expected');
+            })
+            .catch(function (err) {
+                assert(err instanceof PluginError);
+                assert.strictEqual(err.message, "  * There are uncommitted changes in the working tree.");
+            });
+    });
+
+    it('Should pass validation if option is disabled', function () {
+        return cmd('git checkout master')
+            .then(function () {
+                writeFile('README.md', 'Yo!');
+
+                return publish({
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false,
+                    validateBranch:   false
+                });
+            });
+    });
+
+    it('Should pass validation', function () {
+        return cmd('git checkout master')
+            .then(function () {
+                return publish({
+                    confirm:          false,
+                    checkUncommitted: false,
+                    validateGitTag:   false,
+                    validateBranch:   false
                 });
             });
     });
