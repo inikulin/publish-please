1fdef7f1e6db92c1d39dcc4728f865aeef5de806
Henri Le Barrois d'Orgeval <HDO@MacBook.local>
2018-06-28 23:26:34 +0200

    refactor(publish): extract more code to  new module

--- src/publish/publish-prerequisites.js
+++ src/publish/publish-prerequisites.js
@@ -1,23 +1,17 @@
 'use strict';
-const semver = require('semver');
 const node = require('../utils/get-node-infos');
 
 module.exports.assertNode6PublishingPrerequisite = assertNode6PublishingPrerequisite;
 
 // NOTE: adopted from https://github.com/sindresorhus/np/blob/master/index.js#L78
 function assertNode6PublishingPrerequisite() {
-    return node.getCurrentNodeAndNpmVersions().then((versions) => {
-        const isNode6 = semver.gte(versions.node, '6.0.0');
-        const isSafeNpmVersion = semver.satisfies(
-            versions.npm,
-            '>=2.15.8 <3.0.0 || >=3.10.1'
-        );
-        if (isNode6 && isSafeNpmVersion) {
+    return node.getCurrentNodeAndNpmVersions().then((version) => {
+        if (version.isNode6 && version.isSafeNpmVersion) {
             return Promise.resolve();
         }
         throw new Error(
             `npm@${
-                versions.npm
+                version.npm
             } has known issues publishing when running Node.js 6. Please upgrade npm or downgrade Node and publish again. See: https://github.com/npm/npm/issues/5082`
         );
     });
--- src/utils/get-node-infos.js
+++ src/utils/get-node-infos.js
@@ -1,5 +1,7 @@
 'use strict';
 const exec = require('cp-sugar').exec;
+const semver = require('semver');
+const executionContext = require('./execution-context');
 
 module.exports = {
     getCurrentNpmVersion,
@@ -17,6 +19,28 @@ function getCurrentNodeVersion() {
 
 function getCurrentNodeAndNpmVersions() {
     return Promise.all([getCurrentNodeVersion(), getCurrentNpmVersion()]).then(
-        (results) => ({ node: results[0], npm: results[1] })
+        (results) => {
+            const versions = { node: results[0], npm: results[1] };
+
+            const isNode6 = semver.gte(versions.node, '6.0.0');
+            versions.isNode6 = isNode6;
+
+            const isSafeNpmVersion = semver.satisfies(
+                versions.npm,
+                '>=2.15.8 <3.0.0 || >=3.10.1'
+            );
+            versions.isSafeNpmVersion = isSafeNpmVersion;
+
+            const isPrePublishOnlyNpmVersion = semver.gte(
+                versions.npm,
+                '5.6.0'
+            );
+            versions.isPrePublishOnlyNpmVersion = isPrePublishOnlyNpmVersion;
+            if (executionContext.isInTestMode()) {
+                console.log('versions:');
+                console.log(versions);
+            }
+            return Promise.resolve(versions);
+        }
     );
 }
