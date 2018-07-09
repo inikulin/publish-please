de1aca9308b4c16f48aa5159fc6ebd82dd539e33
Henri Le Barrois d'Orgeval <HDO@MacBook.local>
2018-07-10 00:06:58 +0200

    test(npx): add unit tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,6 +6,7 @@ const writeFile = require('fs').writeFileSync;
 const should = require('should');
 const cli = require('../lib');
 const pathJoin = require('path').join;
+const packageName = require('./utils/publish-please-version-under-test');
 
 /** !!!!!!!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  * These tests runs publish-please code on publish-please repo itself
@@ -19,7 +20,14 @@ describe('Publish-please CLI Options', () => {
     let exitCode;
     let output;
     let hasAddedArg;
+    let originalConfiguration;
 
+    before(() => {
+        originalConfiguration = JSON.parse(readFile('.publishrc').toString());
+    });
+    after(() => {
+        writeFile('.publishrc', JSON.stringify(originalConfiguration, null, 2));
+    });
     beforeEach(() => {
         hasAddedArg = false;
         process.env.PUBLISH_PLEASE_TEST_MODE = true;
@@ -50,9 +58,7 @@ describe('Publish-please CLI Options', () => {
             process.argv.pop();
         }
 
-        const publishrc = JSON.parse(readFile('.publishrc').toString());
-        publishrc.prePublishScript = "echo 'npm test'";
-        writeFile('.publishrc', JSON.stringify(publishrc, null, 2));
+        writeFile('.publishrc', JSON.stringify(originalConfiguration, null, 2));
     });
 
     it('Should execute dry-run workflow on `npm run publish-please --dry-run`', () => {
@@ -70,7 +76,35 @@ describe('Publish-please CLI Options', () => {
         );
     });
 
-    it('Should execute dry-run workflow on `npx publish-please --dry-run`', () => {
+    it('Should execute dry-run workflow with no errors on `npm run publish-please --dry-run`', () => {
+        // Given
+        process.env['npm_config_argv'] =
+            '{"remain":[],"cooked":["run","publish-please","--dry-run"],"original":["run","publish-please","--dry-run"]}';
+        const publishrc = JSON.parse(readFile('.publishrc').toString());
+        publishrc.confirm = false;
+        publishrc.validations.uncommittedChanges = false;
+        publishrc.validations.gitTag = false;
+        publishrc.validations.branch = false;
+        writeFile('.publishrc', JSON.stringify(publishrc, null, 2));
+
+        // When
+        return (
+            cli()
+                // Then
+                .then(() => {
+                    output.should.not.containEql('ERRORS');
+                    output.should.containEql('dry mode activated');
+                    output.should.containEql('Running pre-publish script');
+                    output.should.containEql('Running validations');
+                    output.should.containEql('Release info');
+                    output.should.containEql(
+                        "run 'npm pack' to have more details on the package"
+                    );
+                })
+        );
+    });
+
+    it('Should execute dry-run workflow with errors on `npx publish-please --dry-run`', () => {
         // Given
         process.env['npm_config_argv'] = undefined;
 
@@ -95,8 +129,54 @@ describe('Publish-please CLI Options', () => {
         return (
             cli()
                 // Then
-                .catch((err) => err.message.should.containEql('ERRORS'))
-                .catch((err) => output.should.containEql('dry mode activated'))
+                .catch((err) => {
+                    output.should.containEql('dry mode activated');
+                    output.should.containEql('ERRORS');
+                })
+        );
+    });
+
+    it('Should execute dry-run workflow with no errors on `npx publish-please --dry-run`', () => {
+        // Given
+        process.env['npm_config_argv'] = undefined;
+
+        // [ '/usr/local/bin/node',
+        //   '/Users/HDO/.npm/_npx/97852/bin/publish-please',
+        //   '--dry-run'
+        // ]
+        process.argv = [
+            pathJoin('usr', 'local', 'bin', 'node'),
+            pathJoin(
+                'Users',
+                'xxx',
+                '.npm',
+                '_npx',
+                '97852',
+                'bin',
+                'publish-please'
+            ),
+            '--dry-run',
+        ];
+        const publishrc = JSON.parse(readFile('.publishrc').toString());
+        publishrc.confirm = false;
+        publishrc.validations.uncommittedChanges = false;
+        publishrc.validations.gitTag = false;
+        publishrc.validations.branch = false;
+        writeFile('.publishrc', JSON.stringify(publishrc, null, 2));
+        // When
+        return (
+            cli()
+                // Then
+                .then(() => {
+                    output.should.not.containEql('ERRORS');
+                    output.should.containEql('dry mode activated');
+                    output.should.containEql('Running pre-publish script');
+                    output.should.containEql('Running validations');
+                    output.should.containEql('Release info');
+                    output.should.containEql(
+                        "run 'npm pack' to have more details on the package"
+                    );
+                })
         );
     });
 
