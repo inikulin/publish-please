450c507958e5e4dfecb9a38d3d98d0e8c8abc317
Henri Le Barrois d'Orgeval <HDO@MacBook.local>
2018-08-04 12:52:14 +0200

    test(npm): try fix tests on npm version < 6

--- test/13-npm-integration-tests.js
+++ test/13-npm-integration-tests.js
@@ -10,10 +10,15 @@ const exec = require('cp-sugar').exec;
 const Promise = require('pinkie-promise');
 const packageName = require('./utils/publish-please-version-under-test');
 const nodeInfos = require('../lib/utils/get-node-infos').getNodeInfosSync();
+const shouldUsePrePublishOnlyScript = nodeInfos.shouldUsePrePublishOnlyScript;
 const lineSeparator = '----------------------------------';
 
 /* eslint-disable max-nested-callbacks */
-describe('npx integration tests', () => {
+describe.only('npm integration tests', () => {
+    const prepublishKey = shouldUsePrePublishOnlyScript
+        ? 'prepublishOnly'
+        : 'prepublish';
+
     function colorGitOutput() {
         const gitColorCommands = [
             'git config color.branch.current blue',
@@ -65,161 +70,83 @@ describe('npx integration tests', () => {
         return Promise.resolve().then(process.chdir('testing-repo'));
     });
 
-    it('Should be able to run publish-please in dry mode (with no .publishrc config file)', () => {
+    it('Should not install globally', () => {
         return Promise.resolve()
+            .then(() => console.log(`> npm install -g ${packageName}`))
+            .then(() =>
+                exec(`npm install -g ../${packageName.replace('@', '-')}.tgz`)
+            )
             .then(() => {
-                const pkg = JSON.parse(readFile('package.json').toString());
-                const scripts = {};
-                scripts.test = 'echo "running tests ..."';
-                pkg.scripts = scripts;
-                writeFile('package.json', JSON.stringify(pkg, null, 2));
+                throw new Error('Promise rejection expected');
             })
-            .then(() => console.log(`> npx ${packageName} --dry-run`))
+            .catch((err) =>
+                assert(err.message.indexOf('node lib/pre-install.js') > -1)
+            );
+    });
+
+    it('Should install locally', () => {
+        return Promise.resolve()
+            .then(() => console.log(`> npm install --save-dev ${packageName}`))
+            .then(() =>
+                exec(
+                    `npm install --save-dev ../${packageName.replace(
+                        '@',
+                        '-'
+                    )}.tgz`
+                )
+            )
+            .then(() => {
+                const cfg = JSON.parse(readFile('package.json').toString());
+
+                assert.strictEqual(
+                    cfg.scripts['publish-please'],
+                    'publish-please'
+                );
+                assert.strictEqual(
+                    cfg.scripts[prepublishKey],
+                    'publish-please guard'
+                );
+                const publishrc = JSON.parse(readFile('.publishrc').toString());
+                assert(publishrc.confirm);
+                assert.strictEqual(publishrc.prePublishScript, 'npm test');
+                assert.strictEqual(publishrc.postPublishScript, '');
+                assert.strictEqual(publishrc.publishCommand, 'npm publish');
+                assert.strictEqual(publishrc.publishTag, 'latest');
+                assert.strictEqual(publishrc.validations.branch, 'master');
+                assert(publishrc.validations.uncommittedChanges);
+                assert(publishrc.validations.untrackedFiles);
+
+                /* prettier-ignore */
+                nodeInfos.isAtLeastNpm6
+                    ? assert(publishrc.validations.vulnerableDependencies === true)
+                    : assert(publishrc.validations.vulnerableDependencies === false);
+
+                assert(publishrc.validations.sensitiveData);
+                assert(publishrc.validations.gitTag);
+                assert.strictEqual(publishrc.validations.branch, 'master');
+            });
+    });
+
+    it('Should be able to use publish-please after installing locally', () => {
+        return Promise.resolve()
+            .then(() => console.log(`> npm install --save-dev ${packageName}`))
             .then(() =>
                 exec(
                     /* prettier-ignore */
-                    `npx ../${packageName.replace('@','-')}.tgz --dry-run > ./publish01.log`
+                    `npm install --save-dev ../${packageName.replace('@','-')}.tgz`
                 )
             )
+            .then(() => console.log('> npm run publish-please'))
+            .then(() => exec('npm run publish-please > ./publish01.log'))
             .then(() => {
                 const publishLog = readFile('./publish01.log').toString();
                 console.log(publishLog);
                 /* prettier-ignore */
-                assert(publishLog.includes('dry mode activated'));
-                /* prettier-ignore */
                 assert(publishLog.includes('Running pre-publish script'));
-                /* prettier-ignore */
-                assert(publishLog.includes('running tests ...'));
                 /* prettier-ignore */
                 assert(publishLog.includes('Running validations'));
-
-                /* prettier-ignore */
-                nodeInfos.isAtLeastNpm6
-                    ? assert(publishLog.includes('Checking for the vulnerable dependencies'))
-                    : assert(!publishLog.includes('Checking for the vulnerable dependencies'));
-
-                /* prettier-ignore */
-                assert(publishLog.includes('ERRORS'));
-                /* prettier-ignore */
-                assert(publishLog.includes('There are uncommitted changes in the working tree.'));
-            });
-    });
-
-    it('Should be able to run publish-please in dry mode (with existing .publishrc config file)', () => {
-        return Promise.resolve()
-            .then(() => {
-                const pkg = JSON.parse(readFile('package.json').toString());
-                const scripts = {};
-                scripts.test = 'echo "running tests ..."';
-                pkg.scripts = scripts;
-                writeFile('package.json', JSON.stringify(pkg, null, 2));
-            })
-            .then(() => {
-                writeFile(
-                    '.publishrc',
-                    JSON.stringify({
-                        confirm: true,
-                        validations: {
-                            vulnerableDependencies: false,
-                            sensitiveData: false,
-                            uncommittedChanges: false,
-                            untrackedFiles: false,
-                            branch: 'master',
-                            gitTag: false,
-                        },
-                        publishTag: 'latest',
-                        prePublishScript:
-                            'echo "running script defined in .publishrc ..."',
-                        postPublishScript: false,
-                    })
-                );
-            })
-            .then(() => console.log(`> npx ${packageName} --dry-run`))
-            .then(() =>
-                exec(
-                    /* prettier-ignore */
-                    `npx ../${packageName.replace('@','-')}.tgz --dry-run > ./publish02.log`
-                )
-            )
-            .then(() => {
-                const publishLog = readFile('./publish02.log').toString();
-                console.log(publishLog);
-                /* prettier-ignore */
-                assert(publishLog.includes('dry mode activated'));
-                /* prettier-ignore */
-                assert(publishLog.includes('Running pre-publish script'));
-                /* prettier-ignore */
-                assert(publishLog.includes('running script defined in .publishrc ...'));
-                /* prettier-ignore */
-                assert(publishLog.includes('Running validations'));
-                /* prettier-ignore */
-                assert(publishLog.includes('Validating branch'));
-                /* prettier-ignore */
-                assert(!publishLog.includes('ERRORS'));
-                /* prettier-ignore */
-                assert(publishLog.includes('Release info'));
-                /* prettier-ignore */
-                assert(publishLog.includes('testing-repo-1.3.77.tgz'));
-                /* prettier-ignore */
-                assert(publishLog.includes("run 'npm pack' to have more details on the package"));
-            });
-    });
-
-    it('Should be able to configure publish-please', () => {
-        return Promise.resolve()
-            .then(() => console.log(`> npx ${packageName} config`))
-            .then(() =>
-                exec(
-                    /* prettier-ignore */
-                    `npx ../${packageName.replace('@','-')}.tgz config > ./publish03.log`
-                )
-            )
-            .then(() => {
-                const publishLog = readFile('./publish03.log').toString();
-                console.log(publishLog);
-            })
-            .then(() => {
-                const publishrc = JSON.parse(readFile('.publishrc').toString());
-                publishrc.confirm.should.be.true();
-                publishrc.prePublishScript.should.equal('npm test');
-                publishrc.postPublishScript.should.equal('');
-                publishrc.publishCommand.should.equal('npm publish');
-                publishrc.publishTag.should.equal('latest');
-                publishrc.validations.branch.should.equal('master');
-                publishrc.validations.uncommittedChanges.should.be.true();
-                publishrc.validations.untrackedFiles.should.be.true();
-
-                nodeInfos.isAtLeastNpm6
-                    ? publishrc.validations.vulnerableDependencies.should.be.true()
-                    : publishrc.validations.vulnerableDependencies.should.be.false();
-
-                publishrc.validations.sensitiveData.should.be.true();
-                publishrc.validations.gitTag.should.be.true();
-                publishrc.validations.branch.should.equal('master');
-            });
-    });
-
-    it('Should be able to run the publishing workflow (with no .publishrc config file)', () => {
-        return Promise.resolve()
-            .then(() => console.log(`> npx ${packageName}`))
-            .then(() =>
-                exec(
-                    /* prettier-ignore */
-                    `npx ../${packageName.replace('@','-')}.tgz > ./publish04.log`
-                )
-            )
-            .then(() => {
-                const publishLog = readFile('./publish04.log').toString();
-                console.log(publishLog);
-                return publishLog;
-            })
-            .then((publishLog) => {
-                /* prettier-ignore */
-                assert(publishLog.includes('Running pre-publish script'));
                 /* prettier-ignore */
                 assert(publishLog.includes('Error: no test specified'));
-                /* prettier-ignore */
-                assert(publishLog.includes('Running validations'));
 
                 /* prettier-ignore */
                 nodeInfos.isAtLeastNpm6
@@ -239,69 +166,79 @@ describe('npx integration tests', () => {
                 /* prettier-ignore */
                 assert(publishLog.includes('ERRORS'));
                 /* prettier-ignore */
+                assert(publishLog.includes('* There are uncommitted changes in the working tree.'));
+                /* prettier-ignore */
                 assert(publishLog.includes('* There are untracked files in the working tree.'));
                 /* prettier-ignore */
                 assert(publishLog.includes("* Latest commit doesn't have git tag."));
             });
     });
 
-    it('Should be able to run the publishing workflow (with .publishrc config file)', () => {
+    it('Should be able to run publish-please in dry mode after installing locally', () => {
         return Promise.resolve()
-            .then(() => {
-                writeFile(
-                    '.publishrc',
-                    JSON.stringify({
-                        confirm: false,
-                        validations: {
-                            vulnerableDependencies: false,
-                            sensitiveData: true,
-                            uncommittedChanges: false,
-                            untrackedFiles: false,
-                            branch: 'master',
-                            gitTag: false,
-                        },
-                        publishTag: 'latest',
-                        prePublishScript:
-                            'echo "running script defined in .publishrc ..."',
-                        postPublishScript: false,
-                    })
-                );
-            })
-            .then(() => console.log(`> npx ${packageName}`))
+            .then(() => console.log(`> npm install --save-dev ${packageName}`))
             .then(() =>
                 exec(
                     /* prettier-ignore */
-                    `npx ../${packageName.replace('@','-')}.tgz > ./publish05.log`
+                    `npm install --save-dev ../${packageName.replace('@','-')}.tgz`
                 )
             )
+            .then(() =>
+                console.log('> edit .publishrc to disable blocking validations')
+            )
             .then(() => {
-                const publishLog = readFile('./publish05.log').toString();
-                console.log(publishLog);
-                return publishLog;
+                const publishrc = JSON.parse(readFile('.publishrc').toString());
+                publishrc.validations.uncommittedChanges = false;
+                publishrc.validations.untrackedFiles = false;
+                publishrc.validations.gitTag = false;
+                writeFile('.publishrc', JSON.stringify(publishrc));
+                return publishrc;
             })
-            .then((publishLog) => {
+            .then((publishrc) => {
+                console.log('');
+                console.log(publishrc);
+                console.log('');
+            })
+            .then(() => console.log('> npm run publish-please --dry-run'))
+            .then(() =>
+                exec('npm run publish-please --dry-run > ./publish02.log')
+            )
+            .then(() => {
+                const publishLog = readFile('./publish02.log').toString();
+                console.log(publishLog);
+                /* prettier-ignore */
+                assert(publishLog.includes('dry mode activated'));
                 /* prettier-ignore */
                 assert(publishLog.includes('Running pre-publish script'));
                 /* prettier-ignore */
-                assert(publishLog.includes('running script defined in .publishrc ...'));
-                /* prettier-ignore */
                 assert(publishLog.includes('Running validations'));
+
                 /* prettier-ignore */
-                assert(publishLog.includes('Checking for the sensitive data in the working tree'));
-                /* prettier-ignore */
-                assert(publishLog.includes('Validating branch'));
+                nodeInfos.isAtLeastNpm6
+                    ? assert(publishLog.includes('Checking for the vulnerable dependencies'))
+                    : assert(!publishLog.includes('Checking for the vulnerable dependencies'));
+
                 /* prettier-ignore */
                 assert(publishLog.includes('Release info'));
                 /* prettier-ignore */
-                assert(publishLog.includes('ERRORS'));
+                assert(publishLog.includes('testing-repo-1.3.77.tgz'));
                 /* prettier-ignore */
-                assert(publishLog.includes('Command `npm` exited with code'));
+                assert(publishLog.includes("run 'npm pack' to have more details on the package"));
             });
     });
 
     if (!nodeInfos.isAtLeastNpm6) {
         it('Should abort the publishing workflow when npm version < 6 and vulnerability check is enabled in .publishrc config file', () => {
             return Promise.resolve()
+                .then(() =>
+                    console.log(`> npm install --save-dev ${packageName}`)
+                )
+                .then(() =>
+                    exec(
+                        /* prettier-ignore */
+                        `npm install --save-dev ../${packageName.replace('@','-')}.tgz`
+                    )
+                )
                 .then(() => {
                     writeFile(
                         '.publishrc',
@@ -322,13 +259,8 @@ describe('npx integration tests', () => {
                         })
                     );
                 })
-                .then(() => console.log(`> npx ${packageName}`))
-                .then(() =>
-                    exec(
-                        /* prettier-ignore */
-                        `npx ../${packageName.replace('@','-')}.tgz > ./publish06.log`
-                    )
-                )
+                .then(() => console.log('> npm run publish-please'))
+                .then(() => exec('npm run publish-please > ./publish06.log'))
                 .then(() => {
                     const publishLog = readFile('./publish06.log').toString();
                     console.log(publishLog);
@@ -352,13 +284,22 @@ describe('npx integration tests', () => {
                 });
         });
 
-        it('Should abort the dry mode workflow when npm version < 6 and vulnerability check is enabled in .publishrc config file', () => {
+        it('Should abort the dry-mode workflow when npm version < 6 and vulnerability check is enabled in .publishrc config file', () => {
             return Promise.resolve()
+                .then(() =>
+                    console.log(`> npm install --save-dev ${packageName}`)
+                )
+                .then(() =>
+                    exec(
+                        /* prettier-ignore */
+                        `npm install --save-dev ../${packageName.replace('@','-')}.tgz`
+                    )
+                )
                 .then(() => {
                     writeFile(
                         '.publishrc',
                         JSON.stringify({
-                            confirm: true,
+                            confirm: false,
                             validations: {
                                 vulnerableDependencies: true,
                                 sensitiveData: false,
@@ -374,16 +315,16 @@ describe('npx integration tests', () => {
                         })
                     );
                 })
-                .then(() => console.log(`> npx ${packageName} --dry-run`))
+                .then(() => console.log('> npm run publish-please --dry-run'))
                 .then(() =>
-                    exec(
-                        /* prettier-ignore */
-                        `npx ../${packageName.replace('@','-')}.tgz --dry-run > ./publish07.log`
-                    )
+                    exec('npm run publish-please --dry-run > ./publish07.log')
                 )
                 .then(() => {
                     const publishLog = readFile('./publish07.log').toString();
                     console.log(publishLog);
+                    return publishLog;
+                })
+                .then((publishLog) => {
                     /* prettier-ignore */
                     assert(publishLog.includes('dry mode activated'));
                     /* prettier-ignore */
