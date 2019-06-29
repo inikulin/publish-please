0584c15d3719962bb9321ccfcc9489073ee94840
hdorgeval <henri.dorgeval@outlook.fr>
2019-06-29 22:58:38 +0200

    test(publish-with-publish-please): try fix test on CI with npm version 6.9.2

--- test/13-integration-tests.js
+++ test/13-integration-tests.js
@@ -1171,7 +1171,7 @@ describe('Integration tests', () => {
                     return assert(err.message.indexOf(GUARD_ERROR) >= 0);
                 }));
 
-        it('Should allow publishing with special flag', () =>
+        it.only('Should allow publishing with special flag', () =>
             exec('npm publish --with-publish-please')
                 // NOTE: it will reject anyway because this package version already
                 // published or test host don't have permissions to do that
@@ -1179,6 +1179,7 @@ describe('Integration tests', () => {
                     throw new Error('Promise rejection expected');
                 })
                 .catch((err) => {
+                    console.log(err.message);
                     // prettier-ignore
                     assert(
                         err.message.indexOf('You do not have permission to publish') > -1 ||
